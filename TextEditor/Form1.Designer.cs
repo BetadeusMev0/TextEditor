ef59a8b1674cb40b9336331e62f3592d62b8d092
406 pc 2 <you@example.com>
2023-09-30 13:54:12 +0700

    Final

---
+++ TextEditor/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TextEditor
+{
+    partial class TextEditorForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.daasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonContext = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(800, 421);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            this.richTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox_KeyDown);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(129, 6);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 15);
+            this.labelStatus.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(81, 6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Status:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.daasToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.createToolStripMenuItem,
+            this.deleteFileToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(166, 114);
+            // 
+            // daasToolStripMenuItem
+            // 
+            this.daasToolStripMenuItem.Name = "daasToolStripMenuItem";
+            this.daasToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.daasToolStripMenuItem.Text = "Clear F2";
+            this.daasToolStripMenuItem.Click += new System.EventHandler(this.daasToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem1,
+            this.saveToFileToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(172, 22);
+            this.saveToolStripMenuItem1.Text = "Save F5";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            this.saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            this.saveToFileToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.saveToFileToolStripMenuItem.Text = "Save to File ctrl + s";
+            this.saveToFileToolStripMenuItem.Click += new System.EventHandler(this.saveToFileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.openToolStripMenuItem.Text = "Open F4";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.createToolStripMenuItem.Text = "Create catalog F6";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // deleteFileToolStripMenuItem
+            // 
+            this.deleteFileToolStripMenuItem.Name = "deleteFileToolStripMenuItem";
+            this.deleteFileToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.deleteFileToolStripMenuItem.Text = "Delete File F7";
+            this.deleteFileToolStripMenuItem.Click += new System.EventHandler(this.deleteFileToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelStatus);
+            this.panel1.Controls.Add(this.buttonContext);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 23);
+            this.panel1.TabIndex = 2;
+            // 
+            // buttonContext
+            // 
+            this.buttonContext.Location = new System.Drawing.Point(0, 2);
+            this.buttonContext.Name = "buttonContext";
+            this.buttonContext.Size = new System.Drawing.Size(75, 23);
+            this.buttonContext.TabIndex = 14;
+            this.buttonContext.Text = "Options";
+            this.buttonContext.UseVisualStyleBackColor = true;
+            this.buttonContext.Click += new System.EventHandler(this.button1_Click);
+            this.buttonContext.MouseHover += new System.EventHandler(this.buttonContext_MouseHover);
+            // 
+            // TextEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 445);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "TextEditorForm";
+            this.Text = "TextEditor";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox;
+        private Label label1;
+        private Label label2;
+        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openFileDialog1;
+        private Label labelStatus;
+        private Label label9;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem daasToolStripMenuItem;
+        private Panel panel1;
+        private Button buttonContext;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem1;
+        private ToolStripMenuItem saveToFileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem createToolStripMenuItem;
+        private ToolStripMenuItem deleteFileToolStripMenuItem;
+    }
+}
---
+++ TextEditor/Form1.cs
@@ -0,0 +1,195 @@
+using System.Diagnostics.Contracts;
+
+namespace TextEditor
+{
+    public partial class TextEditorForm : Form
+    {
+        public TextEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text= string.Empty;
+        }
+
+        private string path = "";
+
+        private string tmp = "";
+
+
+        private void richTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S) SaveToFile(); 
+            switch (e.KeyCode) 
+            {
+                case Keys.F1:
+                    Point point = this.Location;
+                    point.X += 8;
+                    point.Y += 30 + buttonContext.Height;
+                    contextMenuStrip1.Show(point);
+                    break;
+                case Keys.F2:
+                    richTextBox.Text = string.Empty;
+                    break;
+                case Keys.F4:
+                    WriteOpenFile();
+                    break;
+                case Keys.F5:
+                    SaveFile();
+                    break;
+                case Keys.F6:
+                    CreateCatalog();
+                    break;
+
+                case Keys.F7:
+                    DeleteFile();
+                    break;
+
+                case Keys.Enter:
+                    if (isCreate)
+                    {
+                        Directory.CreateDirectory(richTextBox.Text);
+                        path = richTextBox.Text + "\\" + richTextBox.Text + ".txt";
+                        richTextBox.Text = tmp;
+                        SaveToFile();
+                        labelStatus.Text = "";
+                        isCreate= false;
+                    }
+
+
+                    break;
+            } 
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void SaveFile() 
+        {
+            saveFileDialog1.Filter = "TXT(*.TXT)|*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox.Text);
+                path = saveFileDialog1.FileName;
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            WriteOpenFile();
+        }
+        private void WriteOpenFile() 
+        {
+            openFileDialog1.Filter = "TXT(*.TXT)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.Text = File.ReadAllText(openFileDialog1.FileName);
+                path= openFileDialog1.FileName;
+            }
+        }
+
+        private void buttonSaveToFile_Click(object sender, EventArgs e)
+        {
+            SaveToFile();
+        }
+
+        private void SaveToFile() 
+        {
+            if (path != null && path != "")
+                File.WriteAllText(path, richTextBox.Text);
+        }
+
+        private void buttonCreateCatalog_Click(object sender, EventArgs e)
+        {
+            CreateCatalog();
+        }
+
+        private void CreateCatalog() 
+        {
+            tmp = richTextBox.Text;
+            labelStatus.Text = "¬ведите им€ каталога";
+            richTextBox.Text = string.Empty;
+            isCreate = true;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DeleteFile();
+        }
+
+        private void DeleteFile() 
+        {
+            File.Delete(path);
+            path = "";
+        }
+
+
+
+        private bool isCreate = false;
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point point = this.Location;
+            point.X += 8;
+            point.Y += 30 + buttonContext.Height;
+            contextMenuStrip1.Show(point);
+        }
+
+        private void daasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = string.Empty;
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void saveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveToFile();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WriteOpenFile();
+
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateCatalog();
+        }
+
+        private void deleteFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DeleteFile();
+        }
+
+        private void buttonContext_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(buttonContext,"f1 дл€ быстрого доступа");
+        }
+
+        private void daasToolStripMenuItem_MouseHover(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
